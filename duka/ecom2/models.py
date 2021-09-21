d849b8fef92660b9dd5e20490c4d832fcd631e0a
smagucha <smagucha@gmail.com>
2021-09-21 13:52:06 +0300

    template

--- duka/ecom2/models.py
+++ duka/ecom2/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Catergory(models.Model):
+	name= models.CharField(max_length = 50, unique= True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	name = models.CharField(max_length = 50, unique= True)
+	price = models.FloatField()
+	discount_price = models.FloatField(blank=True, null=True)
+	category = models.ForeignKey(Catergory, on_delete = models.CASCADE)
+	description = models.TextField()
+	photo = models.ImageField(upload_to='images')
+
--- duka/ecom2/views.py
+++ duka/ecom2/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, Catergory
+import requests
 
 # Create your views here.
 
-def home(request):
-	return HttpResponse('hello world')
+
+
+def listproduct(request):
+	list= Product.objects.all()
+	context={
+		'list': list
+	}
+	return render(request, 'ecom2/listproduct.html', context)
+
+
+def category(request):
+	cate = Catergory.objects.all()
+	values=request.POST.get('q','')
+	x = Catergory.objects.get(name=values)
+	print(values)
+	
+	catefind= x.product_set.all()
+	for i in catefind:
+		print(i)
+	context ={
+		'cate': cate,
+		'catefind': catefind,
+	}
+
+	return render(request, 'ecom2/catergory.html', context)
